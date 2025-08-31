7b0fd8bb0861eda8965be7213433d7ab18752d3b
William Marshall <willmarshall2424@gmail.com>
2025-08-31 16:36:43 -0400

    chore: add Fontawesome

--- react-app/src/components/Header/Header.tsx
+++ react-app/src/components/Header/Header.tsx
@@ -1,16 +1,20 @@
 import Logo from "../Logo"
 import "./Header.css"
 
+
 function Header(){
 
     return (
     <header>
         
         <Logo />
+        <div>
+        
         <a href="">Store</a>
         <a href="">Library</a>
         <a href="#">About</a>
         <a href="#">Settings</a>
+        </div>
     </header>
 
     )
