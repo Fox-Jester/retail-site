0b2ce09e45ffc80fe890484b944ce7a1111d5a0d
William Marshall <willmarshall2424@gmail.com>
2025-09-04 09:17:56 -0400

    style: update SearchBar styles

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,14 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./SearchBar.css";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
-import "./SearchBar.css"
-
-export default function SearchBar(){
-
-
-    return(
-
-        <div className="search-group">
-            <input id="shop-search-bar" type="text" placeholder="Search cool games!"></input>
-            <button id="shop-search-btn">Search</button>
-        </div>
-    )
+export default function SearchBar() {
+  return (
+    <div className="search-group">
+      <input
+        id="shop-search-bar"
+        type="text"
+        placeholder="Search cool games!"
+      ></input>
+      <button id="shop-search-btn">
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+      </button>
+    </div>
+  );
 }
